ab67aaf0e32bd9d14c1d6e006225cec530704f2e
Pedro Paulo <pedroppaulo42@gmail.com>
2020-11-30 20:02:30 -0300

    Implementando atualização parcial com PUT

--- src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
@@ -66,6 +66,11 @@ public class PessoaResource {
 		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
 		return ResponseEntity.ok(pessoaSalva);
 	}
+	
+	@PutMapping("/{codigo}/ativo")
+	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
+		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+	}
 }
 
 
